613190cacdd4a79ae35b3c13115286a13b52b2ec
Emily Yeh <e_yeh1@u.pacific.edu>
2023-04-09 12:30:19 -0700

    Created Project 4b. Setup to add 2 input files and contents of both input files append to individual lists

---
+++ project4_singleCycleProcessorSimulator/Project4b_yeh.py
@@ -0,0 +1,142 @@
+'''
+Python MIPS simulator that executes a subset of MIPS machine instructions
+Valid Instructions for part A: add, sub, addi
+Valid Instructions for part B: add, sub, addi, beq, bne, lw, sw
+
+Run PART B using:
+python3 Project4b_yeh.py test0.bin memory.txt
+
+out_control.txt:
+- 10 bits specifying control signals for each clock cycle
+- order of control signals: RegDst  ALUSrc  MemtoReg  RegWrite  MemRead  MemWrite  Branch  ALUOp1  ALUOp2  ZEROBIT
+
+out_registers.txt:
+- PC value in decimal followed by decimal values of first seven registers for each clk cycle
+- format: <pc val>|<reg 0 val>|<reg 1 val>,..<reg 7 val>\n
+
+out_memory.txt:
+- final contents of memory, one word per line
+'''
+
+import sys
+
+def writeCurrentPCReg(pcVal, regVals):
+    pcRegStr = str(pcVal)
+    for i in regVals:
+        pcRegStr += "|" + str(i)
+    return pcRegStr
+
+def main():
+    binInstr = []
+    memVals = []
+
+    controlSignal = ""
+    pcRegVals = ""
+    pc = 65536
+    registers = [0,0,0,0,0,0,0,0]
+    rs = rt = rd = -1
+    imm = None
+    lineNum = 0
+
+    # open bin file and store each line in a list
+    inpFileName = sys.argv[1]
+    binFile = open(inpFileName, "r")
+    for line in binFile:
+        binInstr.append(line.strip())
+    binFile.close()
+    print(binInstr)
+    print(type(binInstr))
+
+    # open memory file and store each line in a list
+    inpFileName = sys.argv[2]
+    memFile = open(inpFileName, "r")
+    for line in memFile:
+        try:
+            memVals.append(int(line.strip()))
+        except:
+            pass
+    memFile.close()
+    print(memVals)
+    print(type(memVals[0]))
+
+'''
+    # initialize pc and register values
+    pcRegVals += writeCurrentPCReg(pc, registers) + "\n"
+
+    # open input file
+    f = sys.argv[1]
+    fileIn = open(f, "r")
+    
+    for line in fileIn:
+        # should the program determine that more than 100 lines have been written,
+        # the program should close the files and exit gracefully (this is unlikely)
+        if lineNum <= 100: 
+            line = line.split()
+            pc += 4
+
+            # extract instruction opcode, rs, and rt
+            instrOp = line[0][0:6]
+            rs = int( line[0][6:11] , 2 ) # typecast binary reg val to dec
+            rt = int( line[0][11:16] , 2 ) # typecast binary reg val to dec
+
+            # Check if r-type instr
+            if instrOp == "000000":
+                # set control signal
+                controlSignal += "1001000100"
+
+                # extract instruction func code & rd
+                instrFunc = line[0][26:32]
+                rd = int( line[0][16:21] , 2 ) # typecast binary reg val to dec
+
+                if instrFunc == "100000":
+                    #print("add   rd ", rd, " rs ", rs, " rt ", rt)
+                    # perform instruction on appropriate registers
+                    registers[rd] = registers[rs] + registers[rt]
+                elif instrFunc == "100010":
+                    #print("sub   rd ", rd, " rs ", rs, " rt ", rt)
+                    # perform instruction on appropriate registers
+                    registers[rd] = registers[rs] - registers[rt]
+
+            # Check if i-type instr (assuming only addi instr is valid)
+            elif instrOp == "001000":
+                # set control signal
+                controlSignal += "0101000000"
+
+                # extract instruction imm val
+                # imm is twos comp
+                # if first bit is 0, convert to dec normally
+                # otherwise flip bits and add 1
+                binNum = line[0][16:32]
+                if binNum[0] == "0":
+                    imm = int( binNum , 2 )
+                elif binNum[0] == "1":
+                    # flip bits
+                    binNum = binNum.replace("0", "2")
+                    binNum = binNum.replace("1", "0")
+                    binNum = binNum.replace("2", "1")
+                    # add 1 and set imm val
+                    imm = (int(binNum,2) + 1) * -1 
+                #print("addi  rt ", rt, " rs ", rs, " imm ", imm)
+
+                # perform addi instr on appropriate registers
+                registers[rt] = registers[rs] + imm
+
+            controlSignal += "\n"
+            pcRegVals += writeCurrentPCReg(pc, registers) + "\n"
+            lineNum += 1
+
+    fileIn.close()
+
+    # create and fill output files
+    fileControlOut = open("out_control.txt", "w")
+    fileRegOut = open("out_registers.txt", "w")
+
+    fileControlOut.write(controlSignal)
+    fileRegOut.write(pcRegVals)
+
+    fileControlOut.close()
+    fileRegOut.close()
+'''
+
+if __name__ == "__main__":
+    main()
