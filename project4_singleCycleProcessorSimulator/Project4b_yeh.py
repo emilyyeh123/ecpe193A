7da6408affe9bc0171b74decb3692c75b1a457fb
Emily Yeh <e_yeh1@u.pacific.edu>
2023-04-13 15:52:10 -0700

    project 4b complete. need to clean up print statements.

--- project4_singleCycleProcessorSimulator/Project4b_yeh.py
+++ project4_singleCycleProcessorSimulator/Project4b_yeh.py
@@ -66,32 +66,36 @@ def main():
 
     # seg fault if pc out of bounds or if memory out of bounds
     while not segFault:
-        pc += 4
+        pc += 4 # increment pc for current instr
 
         # extract instruction opcode, rs, and rt
         instrOp = binInstr[lineNum][0:6]
         rs = int( binInstr[lineNum][6:11] , 2 ) # typecast binary reg val to dec
         rt = int( binInstr[lineNum][11:16] , 2 ) # typecast binary reg val to dec
         #print(pc, " | ", lineNum, "\t\t", instrOp, rs, rt)
-        print(pc, " | ", lineNum, end = "\t\t")
+        #print(pc, " | ", lineNum, end = "\t\t")
 
         # Check if r-type instr
         if instrOp == "000000":
             # set control signal
-            controlSignal += "100100010"
+            controlSignal += "1001000100"
 
             # extract instruction func code & rd
             instrFunc = binInstr[lineNum][26:32]
             rd = int( binInstr[lineNum][16:21] , 2 ) # typecast binary reg val to dec
 
             if instrFunc == "100000":
-                print("add   rd ", rd, " rs ", rs, " rt ", rt)
+                print("add  $", rd, ", $", rs, ", $", rt)
                 # perform instruction on appropriate registers
                 registers[rd] = registers[rs] + registers[rt]
             elif instrFunc == "100010":
-                print("sub   rd ", rd, " rs ", rs, " rt ", rt)
+                print("sub  $", rd, ", $", rs, ", $", rt)
                 # perform instruction on appropriate registers
                 registers[rd] = registers[rs] - registers[rt]
+            
+            if registers[rd] == 0:
+                # set ALU zero bit high
+                controlSignal = controlSignal[:-1] + "1"
 
         # otherwise, must be i-type instr
         else:
@@ -112,17 +116,22 @@ def main():
 
             # check if addi instr
             if instrOp == "001000":
-                print("addi  rt ", rt, " rs ", rs, " imm ", imm)
+                print("addi $", rt, ", $", rs, ", ", imm)
                 # set control signal
-                controlSignal += "010100000"
+                controlSignal += "0101000000"
                 # perform addi instr on appropriate registers
                 registers[rt] = registers[rs] + imm
 
+                if registers[rt] == 0:
+                    # set ALU zero bit high
+                    controlSignal = controlSignal[:-1] + "1"
+
+
             # check if beq instr
             elif instrOp == "000100":
-                print("beq  rs ", rs, " rt ", rt, " label ", imm)
+                print("beq  $", rs, ", $", rt, ",", imm, end = "\t")
                 # set control signal
-                controlSignal += "X0X000101"
+                controlSignal += "X0X0001010"
                 # perform instr, update pc & lineNum as necessary 
                 if registers[rt] == registers[rs]:
                     lineNum += imm
@@ -131,11 +140,15 @@ def main():
                 else:
                     print("\t beq false, continue to next instr")
 
+                if registers[rt] - registers[rs] == 0:
+                    # set ALU zero bit high
+                    controlSignal = controlSignal[:-1] + "1"
+
             # check if bne instr
             elif instrOp == "000101":
-                print("bne  rs ", rs, " rt ", rt, " label ", imm)
+                print("bne  $", rs, ", $", rt, ",", imm, end = "\t")
                 # set control signal
-                controlSignal += "X0X000111"
+                controlSignal += "X0X0001110"
                 # perform instr, update pc & lineNum as necessary 
                 if registers[rt] != registers[rs]:
                     lineNum += imm
@@ -144,35 +157,49 @@ def main():
                 else:
                     print("\t beq false, continue to next instr")
 
+                if registers[rt] - registers[rs] == 0:
+                    # set ALU zero bit high
+                    controlSignal = controlSignal[:-1] + "1"
+
             # check if lw instr
             elif instrOp == "100011":
-                print("lw  rt ", rt, " imm(rs) ", imm, "(", rs, ")")
+                print("lw   $", rt, ",", imm, "( $", rs, ")", end = "\t")
                 # set control signal
-                controlSignal += "011110000"
+                controlSignal += "0111100000"
                 # perform instr, get memory address, update register
                 memAddress = int( (registers[rs] + imm)/4 ) # must segfault if invalid int
                 registers[rt] = memVals[memAddress]
-                print("\t accessing memVal[", memAddress, "] = ", memVals[memAddress],"; reg[", rt,"] = ", registers[rt])
+                print("\t accessing memVal[", memAddress, "] =", memVals[memAddress],"; reg[", rt,"] =", registers[rt])
+
+                if memAddress == 0:
+                    # set ALU zero bit high
+                    controlSignal = controlSignal[:-1] + "1"
 
             # check if sw instr
             elif instrOp == "101011":
-                print("sw  rt ", rt, " imm(rs) ", imm, "(", rs, ")")
+                print("sw   $", rt, ",", imm, "( $", rs, ")", end = "\t")
                 # set control signal
-                controlSignal += "X1X001000"
+                controlSignal += "X1X0010000"
                 # perform instr, get memory address, update val in memory
                 memAddress = int( (registers[rs] + imm)/4 ) # must segfault if invalid int
                 memVals[memAddress] = registers[rt]
-                print("\t accessing reg[", rt,"] = ", registers[rt], "; memVal[", memAddress, "] = ", memVals[memAddress])
+                print("\t accessing reg[", rt,"] =", registers[rt], "; memVal[", memAddress, "] =", memVals[memAddress])
+
+                if memAddress == 0:
+                    # set ALU zero bit high
+                    controlSignal = controlSignal[:-1] + "1"
 
         controlSignal += "\n"
         pcRegVals += writeCurrentPCReg(pc, registers) + "\n"
 
         if lineNum == numOfInstructions-1 and pc == pcMax:
+            # exit loop cleanly if end of file achieved
             break
         elif pc < pcInit or  pc > pcMax or lineNum >= 100:
+            # set seg fault flag
             segFault = True
             break
-        lineNum += 1
+        lineNum += 1 # increment line number for next instr
 
     if segFault == True:
         controlSignal += "!!! Segmentation Fault !!!\r\n"
